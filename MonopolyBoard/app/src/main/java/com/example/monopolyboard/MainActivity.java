67a00cba1a96364d99a80ee04dda13d9146d1a69
jordan <jdeags@gmail.com>
2019-09-29 10:55:40 -0400

    monopoly-esque board

---
+++ MonopolyBoard/app/src/main/java/com/example/monopolyboard/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.monopolyboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
