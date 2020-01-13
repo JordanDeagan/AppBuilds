1b7cf6c0df7f030f61e1c69b83c92693a69b8197
jordan <jdeags@gmail.com>
2020-01-12 23:13:43 -0500

    removed build requirement

--- GamePassword/app/src/main/java/com/example/gamepassword/Scrabble.java
+++ GamePassword/app/src/main/java/com/example/gamepassword/Scrabble.java
@@ -1,13 +1,10 @@
 package com.example.gamepassword;
 
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
-import androidx.annotation.RequiresApi;
 
 public class Scrabble extends GameBase {
 
@@ -15,7 +12,6 @@ public class Scrabble extends GameBase {
     EditText[] board;
     ScrabbleObject gameBoard;
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
